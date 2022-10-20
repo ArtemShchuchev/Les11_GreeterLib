c852742f8dfec92c17d27084bb85efa2fbd3fa32
ArtemShchuchev <shuaa@rambler.ru>
2022-10-20 23:07:56 +0700

    Добавьте файлы проекта.

---
+++ GreeterClient/GreeterClient.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "..\GreeterLib\Greeter.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");   // задаём русский текст
+	system("chcp 1251");            // настраиваем кодировку консоли
+	std::system("cls");
+
+	std::cout << "Задача 1. Статическая библиотека\n";
+	std::cout << "--------------------------------\n" << std::endl;
+	Greeter gr;
+	std::string userName;
+	std::cout << "Введите имя: "; std::cin >> userName;
+	std::cout << gr.greet(userName) << std::endl;
+
+	system("pause"); // Команда задержки экрана
+
+	return 0;
+}
+
+/*
+Пример работы программы
+Консоль
+Введите имя: Владислав
+Здравствуйте, Владислав!
+*/
---
+++ GreeterLib/Greeter.cpp
@@ -0,0 +1,6 @@
+#include "Greeter.h"
+
+std::string Greeter::greet(std::string s)
+{
+    return (HELLO_STR + s + "!");
+}
